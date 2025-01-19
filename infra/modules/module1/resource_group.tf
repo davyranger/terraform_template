4092e47b9b4f6fff5f629d71279dbc06a84dfee6
davy <davy.strain@hotmail.com>
2025-01-19 10:00:18 +0800

    added another storage account for testing

--- infra/modules/module1/resource_group.tf
+++ infra/modules/module1/resource_group.tf
@@ -2,23 +2,3 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name # Name of the resource group
   location = var.location            # Azure region where the resource group is located
 }
-
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestoraccv2"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  depends_on = [ azurerm_resource_group.rg ]
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestoraccv3"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  depends_on = [ azurerm_resource_group.rg ]
-}
